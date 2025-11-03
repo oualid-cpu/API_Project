8e0eb9db62b0a8d0ea74db4a969f31b0251fff61
JoshSald <42002468+JoshSald@users.noreply.github.com>
2025-11-03 18:03:23 +0100

    feat: add Date to cards

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,8 +3,22 @@ import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { getImages } from "@/lib/imageapi";
 import { Link } from "react-router-dom";
 
-export default function EventCard({ id, title, description }) {
+export default function EventCard({ id, title, description, date, location }) {
   const [eventImage, setEventImage] = useState();
+  function formatDateSplit(dateString) {
+    const date = new Date(dateString);
+
+    const day = date.toLocaleDateString("en-GB", { day: "2-digit" }); // "24"
+    const month = date.toLocaleDateString("en-GB", { month: "short" }); // "aug"
+    const year = date.getFullYear(); // 2025
+
+    return {
+      firstLine: `${day} ${month}`, // "24aug"
+      secondLine: `${year}`, // "2025"
+    };
+  }
+
+  const { firstLine, secondLine } = formatDateSplit(date);
   useEffect(() => {
     (async () => {
       try {
@@ -17,17 +31,21 @@ export default function EventCard({ id, title, description }) {
   }, []);
   return (
     <CardContainer className="inter-var">
-      <CardBody className="relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border  ">
-        <CardItem translateZ="50" className="text-xl font-bold text-white">
-          {title}
-        </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="text-sm max-w-sm mt-2 text-neutral-300"
-        >
-          {description}
-        </CardItem>
+      <CardBody className="relative group/card flex flex-col h-full hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black/80 border-white/[0.2] border-black/[0.1] w-auto rounded-xl p-6 border">
+        <div className="card-headline flex justify-between items-start">
+          <div className="flex flex-col">
+            <CardItem translateZ="50" className="text-xl font-bold text-white">
+              {title}
+            </CardItem>
+            <CardItem translateZ="30" className="text-sm  text-white/70">
+              {location}
+            </CardItem>
+          </div>
+          <div className=" bg-teal text-white text-xs px-2 py-4 rounded-md shadow flex flex-col items-center">
+            <p className="font-extrabold">{firstLine}</p>
+            <p className="">{secondLine}</p>
+          </div>
+        </div>
         <CardItem translateZ="100" className="w-full mt-4">
           <img
             src={eventImage}
@@ -37,7 +55,14 @@ export default function EventCard({ id, title, description }) {
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-sm max-w-sm mt-2 text-neutral-300 line-clamp-3"
+        >
+          {description}
+        </CardItem>
+        <div className="flex justify-between items-center mt-8">
           <CardItem
             translateZ={20}
             as="button"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,15 +14,17 @@ export default function Home() {
   }, []);
 
   return (
-    <section id="homePage pt-6">
+    <section id="homePage">
       <h1 className="text-3xl font-bold mt-6">Upcoming Events</h1>
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-3 gap-5 items-stretch">
         {events.map((event) => (
           <EventCard
-            key={(event.id * 10202) / 32}
+            key={event.id}
             id={event.id}
             title={event.title}
             description={event.description}
+            date={event.startDate}
+            location={event.location}
           />
         ))}
       </div>
